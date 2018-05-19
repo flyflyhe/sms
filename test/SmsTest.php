4b23ba71c56617384f7b54c600598296225156d6
meHe <meHe>
2018-05-19 15:28:21 +0800

    test init

---
+++ test/SmsTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace sms\test;
+
+use PHPUnit\Framework\TestCase;
+
+require '../vendor/autoload.php';
+class SmsTest extends TestCase
+{
+    protected $config;
+
+    public function setUp()
+    {
+
+    }
+}
